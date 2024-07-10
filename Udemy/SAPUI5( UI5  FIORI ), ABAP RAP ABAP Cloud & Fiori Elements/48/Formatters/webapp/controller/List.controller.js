02bd530a842ea628a6bf4107b5ce6285840c2c0c
Abhay Singh <abhaybsingh19w33@gmail.com>
2024-07-10 11:48:04 +0530

    UI5 48

--- Udemy/SAPUI5( UI5  FIORI ), ABAP RAP ABAP Cloud & Fiori Elements/48/Formatters/webapp/controller/List.controller.js
+++ Udemy/SAPUI5( UI5  FIORI ), ABAP RAP ABAP Cloud & Fiori Elements/48/Formatters/webapp/controller/List.controller.js
@@ -1,5 +1,5 @@
 sap.ui.define(["sap/ui/core/mvc/Controller", "sap/ui/core/routing/History", "sap/m/MessageToast", "satya/prasad/mvcapp/utilities/utilities"],
-	function (Controller, History,
+	function(Controller, History,
 		MessageToast, utilities) {
 		"use strict";
 		return Controller.extend("satya.prasad.mvcapp.controller.List", {
