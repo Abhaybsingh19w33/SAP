2174a5b25f98175050ca08540a370f9c3cb052d6
Abhay Singh <abhaybsingh19w33@gmail.com>
2024-09-18 20:15:54 +0530

    Step 15: Nested Views

--- SAP UI5 - Fiori - FreeCodeCamp/webapp/controller/App.controller.js
+++ SAP UI5 - Fiori - FreeCodeCamp/webapp/controller/App.controller.js
@@ -1,18 +1,7 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], function (Controller, MessageToast) {
+    "sap/ui/core/mvc/Controller"
+], function (Controller) {
     "use strict"
     return Controller.extend("sap.ui.demo.walkthrough.App", {
-        onShowHello: function () {
-
-            console.log("-------------App.Component.js onShowHello func called-------------");
-            // read msg from i18n model
-            var oBundle = this.getView().getModel("i18n").getResourceBundle();
-            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-            var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-            // Show message
-            MessageToast.show(sMsg);
-        }
     });
 });
